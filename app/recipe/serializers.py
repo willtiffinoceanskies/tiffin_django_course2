06904710890ea614c902ef2c3c18232c33ff8f5a
Will Tiffin <willtiffin@oceanskies.com>
2023-08-30 11:30:31 +0100

    --Added files

---
+++ app/recipe/serializers.py
@@ -0,0 +1,16 @@
+'''
+Serializers for recipe APIS
+'''
+
+from rest_framework import serializers
+
+from core.models import Recipe
+
+class RecipeSerializer(serializers.ModelSerializer):
+	'''Serializers for recipes.'''
+
+	class Meta:
+		model = Recipe,
+		fields = ['id','title','time_minutes','price','link']
+		read_only_fields = ['id']
+
